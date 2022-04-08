e42dbabbf89c6b5b9d59e8dc8c091120ebc061e4
Ragu Neopaney <raguneopaney@gmail.com>
2022-04-08 17:15:27 -0400

    DBConnection and Contact us form done

--- Controllers/ContactUs.cs
+++ Controllers/ContactUs.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinalProjectWebServer.DBContext;
+using FinalProjectWebServer.Models.DomainModels;
+using FinalProjectWebServer.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProjectWebServer.Controllers
 {
     public class ContactUs : Controller
     {
+        // DBContext stuff
+        private ContactUsDbContext _context { get; set; }
+
+        public ContactUs(ContactUsDbContext ctx)
+        {
+            _context = ctx;
+        }
+
         public IActionResult ContactUsHome()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactUsPost(ContactUsViewModel ContactUsViewModelForDB)
+        {
+            if (ModelState.IsValid)
+            {
+                ContactUsDomainModel contactUstosave = new ContactUsDomainModel
+                {
+                    Name = ContactUsViewModelForDB.Name,
+                    Email = ContactUsViewModelForDB.Email,
+                    Messsage = ContactUsViewModelForDB.Message
+                };
+                _context.ContactUs.Add(contactUstosave);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("ContactUsHome", "ContactUs");
         }
     }
 }
---
+++ wwwroot/js/AboutUs.js
@@ -0,0 +1,63 @@
+﻿$('#myForm').submit(function (e) {
+    e.preventDefault();
+    var data = $("#myForm").serialize();
+
+    var isValid = true;
+    $('.form-control').each(function () {
+        var element = $(this);
+        if (element.val() == "") {
+            isValid = false;
+        }
+    });
+    $.ajax({
+        type: "POST",
+        url: "/ContactUs/ContactUsPost",
+        data: data,
+        success: function (response) {
+            if (!response.success === true && isValid) {
+                toastr.success('Sign Up Successful');
+                $('#myForm')[0].reset();
+            }
+        }
+    })
+});
+
+function empty() {
+    var x, y, z;
+    x = document.getElementById("name").value;
+    y = document.getElementById("email").value;
+    z = document.getElementById("message").value;
+    if ((x == "" || x == null) || (y == "" || y == null) || (z == "" || z == null)) {
+        if ((x == "" || x == null) && (y == "" || y == null) && (z == "" || z == null)) {
+            toastr.error("All three field must be filled ");
+            return false;
+        }
+        else if ((y == "" || y == null) && (z == "" || z == null)) {
+            toastr.error("Email and Message must be filled");
+            return false;
+        }
+        else if ((x == "" || x == null) && (y == "" || y == null)) {
+            toastr.error("Name and Email must be filled");
+            return false;
+        }
+        else if ((x == "" || x == null) && (z == "" || z == null)) {
+            toastr.error("Name and Message must be filled");
+            return false;
+        }
+        else if ((x == "" || x == null)) {
+            toastr.error("Name field must be filled ");
+            return false;
+        }
+        else if ((y == "" || y == null)) {
+            toastr.error("Email field must be filled ");
+            return false;
+        }
+        else if ((z == "" || z == null)) {
+            toastr.error("Message field must be filled ");
+            return false;
+        }
+    }
+    else {
+        return true;
+    };
+}
